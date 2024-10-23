a90ca9251d2aba9891e36262928ca711488f1bcf
GustavoFayão <gustavo.fayao@hotmail.com>
2024-10-23 17:15:30 -0300

    Realizando mudanças no campos

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; // Corrigido para ReactiveFormsModule
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, NgIf], // Corrigido aqui também
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'] // Corrigido para styleUrls
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";  
+  loginForm!: FormGroup;
+  constructor(private fb:FormBuilder){ }
+  
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    })
+  }
 
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,3 +6,4 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent }
 ];
+
